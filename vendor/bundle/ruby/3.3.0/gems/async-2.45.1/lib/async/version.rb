f31db7489cbd45ac4b5e9101afc5f3581b0451ce
kali <kali@kali.kali>
2026-09-13 12:18:56 +0300

    Remove vendor directory from git tracking

--- vendor/bundle/ruby/3.3.0/gems/async-2.45.1/lib/async/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2017-2026, by Samuel Williams.
-
-# @namespace
-module Async
-	VERSION = "2.45.1"
-end
--- vendor/bundle/ruby/3.3.0/gems/console-1.37.0/lib/console/version.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2019-2026, by Samuel Williams.
-
-module Console
-	VERSION = "1.37.0"
-end
